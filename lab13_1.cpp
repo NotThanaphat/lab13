eb7ad05bc33c3a2358f309d3e112031701d127fb
NotThanaphat <thanapat.pingkaew@gmail.com>
2026-01-09 11:24:01 +0700

    upload lab13 1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,31 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]){
+    double sum = 0;
+    double sum_sq_diff = 0;
+    double product = 1;
+    double sum_inverse = 0;
+    double max_val = A[0];
+    double min_val = A[0];
+
+    for(int i = 0; i < N; i++){
+        sum += A[i];
+        product *= A[i];
+        sum_inverse += 1.0 / A[i];
+        if(A[i] > max_val) max_val = A[i];
+        if(A[i] < min_val) min_val = A[i];
+    }
+
+    B[0] = sum / N;
+    B[2] = pow(product, 1.0 / N);
+    B[3] = N / sum_inverse;
+    B[4] = max_val;
+    B[5] = min_val;
+
+    for(int i = 0; i < N; i++){
+        sum_sq_diff += pow(A[i] - B[0], 2);
+    }
+    B[1] = sqrt(sum_sq_diff / N);
+}
